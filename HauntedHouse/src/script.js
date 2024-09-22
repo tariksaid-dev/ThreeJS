3bdc84825a9575a6a74b9cfef5a205c3e5446fe1
tariksaid-dev <tariksaid.dev@gmail.com>
2024-09-22 09:40:45 +0200

    Add and optimize shadows

--- HauntedHouse/src/script.js
+++ HauntedHouse/src/script.js
@@ -211,7 +211,7 @@ const door = new THREE.Mesh(
     aoMap: doorAmbientOcclusionTexture,
     displacementMap: doorHeightTexture,
     displacementScale: 0.15,
-    discplacementBias: -0.04,
+    displacementBias: -0.04,
     normalMap: doorNormalTexture,
     metalnessMap: doorMetalnessTexture,
     roughnessMap: doorRoughnessTexture,
@@ -364,6 +364,52 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
+ * Shadows
+ */
+
+//Renderer
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+// Cast and receive
+directionalLight.castShadow = true;
+ghost1.castShadow = true;
+ghost2.castShadow = true;
+ghost3.castShadow = true;
+
+walls.castShadow = true;
+walls.receiveShadow = true;
+roof.castShadow = true;
+floor.receiveShadow = true;
+
+for (const grave of graves.children) {
+  grave.castShadow = true;
+  grave.receiveShadow = true;
+}
+
+// Mapping
+directionalLight.shadow.mapSize.width = 256
+directionalLight.shadow.mapSize.height = 256
+directionalLight.shadow.camera.top = 8
+directionalLight.shadow.camera.right = 8
+directionalLight.shadow.camera.bottom = - 8
+directionalLight.shadow.camera.left = - 8
+directionalLight.shadow.camera.near = 1
+directionalLight.shadow.camera.far = 20
+
+ghost1.shadow.mapSize.width = 256
+ghost1.shadow.mapSize.height = 256
+ghost1.shadow.camera.far = 10
+
+ghost2.shadow.mapSize.width = 256
+ghost2.shadow.mapSize.height = 256
+ghost2.shadow.camera.far = 10
+
+ghost3.shadow.mapSize.width = 256
+ghost3.shadow.mapSize.height = 256
+ghost3.shadow.camera.far = 10
+
+/**
  * Animate
  */
 const timer = new Timer();
@@ -397,7 +443,7 @@ const tick = () => {
     Math.sin(ghost3Angle) *
     Math.sin(ghost3Angle * 2.34) *
     Math.sin(ghost3Angle * 3.45);
-    
+
   // Update controls
   controls.update();
 
